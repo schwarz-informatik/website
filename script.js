f6f90e93c5d661c4078c77c199b16a76dfcc3a19
Thomas Schwarz <thomas@schwarz-informatik.at>
2025-03-21 07:19:41 +0100

    add services

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function burgerclick() {
         document.getElementById("mainNavigation").style.display = "none";
     }
 }
+
+function servicesclick(service) {
+    console.log("services click");
+    console.log(service);
+
+    if (service === "ot") {
+        document.getElementById("wrapperOT").style.display = "flex";
+        document.getElementById("wrapperSE").style.display = "none";
+        document.getElementById("wrapperSecurity").style.display = "none";
+    }
+
+    if (service === "se") {
+        document.getElementById("wrapperOT").style.display = "none";
+        document.getElementById("wrapperSE").style.display = "flex";
+        document.getElementById("wrapperSecurity").style.display = "none";
+    }
+
+    if (service === "security") {
+        document.getElementById("wrapperOT").style.display = "none";
+        document.getElementById("wrapperSE").style.display = "none";
+        document.getElementById("wrapperSecurity").style.display = "flex";
+    }
+}
